d2c4eef383e21da61316568d8abe360d6a826489
is221310 <is221310@fhstp.ac.at>
2025-06-23 20:37:32 +0200

    refactored Models for CoreService

---
+++ SecureArchCore/Models/Sensor.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SecureArchCore.Models
+{
+    public class Sensor
+    {
+        public int sensor_id { get; set; }
+        public string sensor_name { get; set; }
+        public string beschreibung { get; set; }
+        public DateTime created_at { get; set; }
+
+        public int? kunden_id { get; set; }
+        [JsonIgnore]
+        public Kunde? Kunde { get; set; }
+
+        public List<string> ip_addresses { get; set; } = new();
+        public List<Temperatur> Temperaturen { get; set; } = new();
+        public ICollection<IpResult> IpResults { get; set; } = new List<IpResult>();
+
+        public string? secret_key { get; set; }
+    }
+}
