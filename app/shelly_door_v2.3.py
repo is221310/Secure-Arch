f0913e25e8c2624405e7ac702a02379df8e282b8
SuzannahMa <judith.neuhuber@gmail.com>
2025-06-28 09:06:32 +0200

    Update shelly_door_v2.3.py

--- app/shelly_door_v2.3.py
+++ app/shelly_door_v2.3.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 
 # Konfiguration
 load_dotenv()
-BASE_URL = "http://192.168.0.51:5236"
+BASE_URL = "http://192.168.0.50:5236"
 SESSION = requests.Session()
 SESSION.verify = False  # Nur für Selbstsignierte Zertifikate
 
